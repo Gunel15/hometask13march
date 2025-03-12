3b6e320d0342930a610a6e185113b13b8bedf932
unknown <you@example.com>
2025-03-12 09:40:47 -0700

    task 3

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+int[] arr = { 1, 2, 3, 4, 5, 6, };int min = arr[0];int max = arr[0];int sum;
+for (int i = 0; i < arr.Length; i++)
+    if (arr[i] < min)
+    { min = arr[i]; }
+for (int j = 0; j < arr.Length; j++)
+if (max < arr[j])
+{ max = arr[j]; }
+{ sum = min + max; }
+{ Console.WriteLine(sum); }    
+
+
+
