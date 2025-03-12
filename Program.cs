9e5a6159780f3eef2c91826c1802ca6f117ca834
unknown <you@example.com>
2025-03-12 09:59:50 -0700

    task 4

--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
 ﻿
-int[] arr = { 1, 2, 3, 4, 5, 6, };int min = arr[0];int max = arr[0];int sum;
-for (int i = 0; i < arr.Length; i++)
-    if (arr[i] < min)
-    { min = arr[i]; }
-for (int j = 0; j < arr.Length; j++)
-if (max < arr[j])
-{ max = arr[j]; }
-{ sum = min + max; }
-{ Console.WriteLine(sum); }    
+//int[] arr = { 1, 2, 3, 4, 5, 6, };int min = arr[0];int max = arr[0];int sum;
+//for (int i = 0; i < arr.Length; i++)
+//    if (arr[i] < min)
+//    { min = arr[i]; }
+//for (int j = 0; j < arr.Length; j++)
+//if (max < arr[j])
+//{ max = arr[j]; }
+//{ sum = min + max; }
+//{ Console.WriteLine(sum); }    
 
+
+int[] arr = { 1, 2, 3, 4, 5, 9 };int result=0;
+for (int i = 0; i < arr.Length; i++)
+    if (arr[i] % 2 != 0 && arr[i] % 3 == 0)
+    {  result++; }
+    { Console.WriteLine(result++); }
 
 
--- obj/Debug/net8.0/hometask13march.AssemblyInfo.cs
+++ obj/Debug/net8.0/hometask13march.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hometask13march")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b6e320d0342930a610a6e185113b13b8bedf932")]
 [assembly: System.Reflection.AssemblyProductAttribute("hometask13march")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hometask13march")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
