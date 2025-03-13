16dcd52a66857a295799f657a4176dcc8adeb203
unknown <you@example.com>
2025-03-13 14:07:22 -0700

    task 1 hazirdir

--- Program.cs
+++ Program.cs
@@ -7,13 +7,27 @@
 //if (max < arr[j])
 //{ max = arr[j]; }
 //{ sum = min + max; }
-//{ Console.WriteLine(sum); }    
+//{ Console.WriteLine(sum); } task 3   
 
 
-int[] arr = { 1, 2, 3, 4, 5, 9 };int result=0;
-for (int i = 0; i < arr.Length; i++)
-    if (arr[i] % 2 != 0 && arr[i] % 3 == 0)
-    {  result++; }
-    { Console.WriteLine(result++); }
+//int[] arr = { 1, 2, 3, 4, 5, 9 };int result=0;
+//for (int i = 0; i < arr.Length; i++)
+//    if (arr[i] % 2 != 0 && arr[i] % 3 == 0)
+//    {  result++; }
+//    { Console.WriteLine(result++); } task 4
+
+
+int num = 196451, max = 0, reqem;
+while (num > 0)
+
+{
+    reqem = num % 10;
+    if (reqem >= max)
+        max = reqem;
+    num = num / 10;
+
+
+}
+{ Console.WriteLine(max); }
 
 
